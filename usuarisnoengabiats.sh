16fb35b1ebc009fc1970db76c6e88c2aad09f0da
lluisfsegui <lluisfelipsegui@paucasesnovescifp.cat>
2026-02-14 21:28:48 +0100

    Enhance user management and FTP configuration
    Added user groups and modified user creation to assign groups. Configured FTP for anonymous access and set permissions for the directory.

--- usuarisnoengabiats.sh
+++ usuarisnoengabiats.sh
@@ -3,29 +3,55 @@
 apt update
 apt install -y vsftpd
 
-# Crear usuarios (4 líneas, fácil de cambiar)
-useradd -m -s /bin/bash gandalf
-useradd -m -s /bin/bash celeborn
-useradd -m -s /bin/bash radagast
-useradd -m -s /bin/bash peregrin
+# Crear grupos
+groupadd ftp_permisos
+groupadd ftp_sin_permisos
 
-echo "gandalf:1234" | chpasswd
-echo "celeborn:1234" | chpasswd
-echo "radagast:1234" | chpasswd
-echo "peregrin:1234" | chpasswd
+# Crear usuarios y asignarlos a grupos
+useradd -m -s /bin/bash gandalf   -G ftp_permisos
+useradd -m -s /bin/bash celeborn  -G ftp_permisos
+useradd -m -s /bin/bash radagast  -G ftp_sin_permisos
+useradd -m -s /bin/bash peregrin  -G ftp_sin_permisos
 
-# Configuración FTP con usuarios NO engabiados
-cp /etc/vsftpd.conf /etc/vsftpd.conf.bak
+echo "gandalf:12345678" | chpasswd
+echo "celeborn:12345678" | chpasswd
+echo "radagast:12345678" | chpasswd
+echo "peregrin:12345678" | chpasswd
 
+# -------------------------------
+# SEGUNDA PARTE DEL EJERCICIO
+# -------------------------------
+
+# Crear el directorio /var/erebor/anonim
+mkdir -p /var/erebor/anonim
+
+# Asignar el grupo con permisos (gandalf y celeborn)
+chown :ftp_permisos /var/erebor/anonim
+
+# Dar permisos de lectura y escritura al grupo
+chmod 770 /var/erebor/anonim
+
+# -------------------------------
+# TERCERA PARTE DEL EJERCICIO
+# -------------------------------
+
+# Habilitar usuarios anónimos y asignarles el directorio
 cat > /etc/vsftpd.conf << 'EOF'
 listen=YES
 local_enable=YES
 write_enable=YES
-anonymous_enable=NO
 
 # Usuarios NO engabiados
 chroot_local_user=NO
+
+# Habilitar usuarios anónimos
+anonymous_enable=YES
+anon_root=/var/erebor/anonim
+
+# Seguridad básica para anónimos
+anon_upload_enable=NO
+anon_mkdir_write_enable=NO
+anon_other_write_enable=NO
 EOF
 
 systemctl restart vsftpd
-systemctl status vsftpd
